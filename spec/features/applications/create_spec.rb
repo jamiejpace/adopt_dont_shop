be3b21b701b003ea37f736c66f35a9b28e623af7
Jamie Pace <jamiejpace@gmail.com>
2021-09-03 18:38:26 -0600

    Create application new

---
+++ spec/features/applications/create_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe "Application new page" do
+  it 'has a link on the pet index page that takes you to the new application page' do
+    visit '/pets'
+
+    click_link('Start an Application')
+
+    expect(current_path).to eq('/applications/new')
+  end
+
+  it 'has a form to create a new application' do
+    visit '/applications/new'
+
+    fill_in('Name', with: 'Katie Crutchfield')
+    fill_in('Street', with: 'Lilac Ln')
+    fill_in('City', with: 'Saint Cloud')
+    fill_in('State', with: 'Florida')
+    fill_in('Zip code', with: '33128')
+
+    click_button('Submit')
+
+    expect(current_path).to eq("/applications/#{Application.all.last.id}")
+    expect(page).to have_content("Katie Crutchfield")
+    expect(page).to have_content("In Progress")
+  end
+end
