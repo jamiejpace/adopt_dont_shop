77f169897e920a8a38e2c36b13cf0a880e8d9e36
Jamie Pace <jamiejpace@gmail.com>
2021-09-08 16:43:50 -0600

    Fix changes to test file

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -8,6 +8,12 @@ RSpec.describe Application do
 
   describe 'validations' do
     it { should validate_length_of(:zip_code) }
+    it { should validate_presence_of(:street) }
+    it { should validate_presence_of(:city) }
+    it { should validate_presence_of(:state) }
+    it { should validate_presence_of(:zip_code) }
+    it { should validate_numericality_of(:zip_code) }
+    it { should validate_length_of(:zip_code) }
   end
 
   describe 'instance methods' do
